3b99cd0d116a347abc7e4a14035398055b150cc0
hojune <ghwns7022@gmail.com>
2019-03-10 14:49:25 +0900

    새로운 커밋 테스트

---
+++ app/src/main/java/com/neocan/githubtest/Main3Activity.java
@@ -0,0 +1,20 @@
+package com.neocan.githubtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //테스트
+        //테스트 추가
+
+        /**
+         *  RKSKEKFWEOFWEOFOWFEOIWEFOFOFIOWFOWIFOIWEFOIEFWOI
+         */
+        // TEST
+    }
+}
